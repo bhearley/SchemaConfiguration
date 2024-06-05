e90165588b74e824a7a2447c02637a2bb7682921
bhearley <109361610+bhearley@users.noreply.github.com>
2024-06-05 13:49:12 -0400

    Update SchemaConfiguration.py

--- SchemaConfiguration.py
+++ SchemaConfiguration.py
@@ -376,16 +376,16 @@ else:
                 with tab_cols[0]:
                     D["var1_" + str(i)] = st.empty()
                     if i == 0:
-                        col_vals[i] = D["var1_" + str(i)] .text_input('Database Attribute',value = GrantaCols[i], key = f'tab_a_{st.session_state[ct]}')
+                        col_vals[i] = D["var1_" + str(i)] .text_input('Database Attribute',value = GrantaCols[i], key = f'tab_a_{st.session_state["ct"]}')
                     else:
-                        col_vals[i] = D["var1_" + str(i)] .text_input('Database Attribute',value = GrantaCols[i], key = f'tab_a_{st.session_state[ct]}', label_visibility="collapsed")
+                        col_vals[i] = D["var1_" + str(i)] .text_input('Database Attribute',value = GrantaCols[i], key = f'tab_a_{st.session_state["ct"]}', label_visibility="collapsed")
                 with tab_cols[1]:
                     D["var2_" + str(i)]= st.empty()
                     if i == 0:
-                        new_vals[i] = D["var2_" + str(i)].selectbox('Py MI Lab Attribute',JSON_atts,index = None, key = f'tab_b_{st.session_state[ct]}')
+                        new_vals[i] = D["var2_" + str(i)].selectbox('Py MI Lab Attribute',JSON_atts,index = None, key = f'tab_b_{st.session_state["ct"]}')
                         #JSON_atts.index(PyCols[i])
                     else:
-                        new_vals[i] = D["var2_" + str(i)].selectbox('Database Attribute',JSON_atts, index = None, key = f'tab_b_{st.session_state[ct]}', label_visibility="collapsed")
+                        new_vals[i] = D["var2_" + str(i)].selectbox('Database Attribute',JSON_atts, index = None, key = f'tab_b_{st.session_state["ct"]}', label_visibility="collapsed")
             st.session_state["col_names"] = D
             st.session_state["ct"] = st.session_state["ct"]+1
 
