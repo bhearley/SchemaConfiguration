d856836b7cd949a8fd8b1ff60e15e7266ce4ffc4
bhearley <109361610+bhearley@users.noreply.github.com>
2024-06-05 14:44:51 -0400

    Update SchemaConfiguration.py

--- SchemaConfiguration.py
+++ SchemaConfiguration.py
@@ -359,7 +359,8 @@ else:
 
             # time.sleep(0.500)
 
-            
+            st.write(st.session_state['tab_att_opt'])
+            st.write(st.session_state['prev_opt'] )
             if st.session_state['prev_opt'] != st.session_state['tab_att_opt']:
                 st.session_state["ct"] = st.session_state["ct"]+1
                 
@@ -394,8 +395,7 @@ else:
                 st.session_state['prev_opt'] = st.session_state['tab_att_opt']
                 st.session_state["ct"] = st.session_state["ct"]+i
 
-                st.write(st.session_state['tab_att_opt'])
-                st.write(st.session_state['prev_opt'] )
+
 
     update_tab()
 
