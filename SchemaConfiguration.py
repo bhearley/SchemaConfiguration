4494c838255198acb3ce895a14e603132b894cb9
bhearley <109361610+bhearley@users.noreply.github.com>
2024-06-10 10:06:33 -0400

    Update SchemaConfiguration.py

--- SchemaConfiguration.py
+++ SchemaConfiguration.py
@@ -493,7 +493,7 @@ else:
                 st.write(st.session_state['placement_flags'])
                 st.write(st.session_state['single_json'])
                 st.write(idx)
-                st.multiselect('Conditional Attribute', st.session_state['single_json'], default= idx, placeholder = "Select the naming attribute and format", key = f'folder_sec_e_{m}_{n}', label_visibility="collapsed")
+                st.multiselect('Conditional Attribute', st.session_state['single_json'], default= Config['Placement']['Level ' + str(m+1)][n][4] , placeholder = "Select the naming attribute and format", key = f'folder_sec_e_{m}_{n}', label_visibility="collapsed")
             with grid_sec[5]:
                 # Determine if a value previously exists
                 idx = '[attribute]'
