2aa948f71a7a67f1f407686a8f2a49c03d109597
bhearley <109361610+bhearley@users.noreply.github.com>
2024-06-10 10:21:05 -0400

    Update SchemaConfiguration.py

--- SchemaConfiguration.py
+++ SchemaConfiguration.py
@@ -467,7 +467,8 @@ else:
                     if 'placement_flags' in st.session_state:
                         if st.session_state['placement_flags'] > 0:
                             if 'Level ' + str(m+1) in list(Config['Placement'].keys()):
-                                idx = st.session_state['single_json'].index(Config['Placement']['Level ' + str(m+1)][n][1])
+                                if Config['Placement']['Level ' + str(m+1)][n][1] != None:
+                                    idx = st.session_state['single_json'].index(Config['Placement']['Level ' + str(m+1)][n][1])
                     st.selectbox('Conditional Attribute', st.session_state['single_json'], index=idx, placeholder = "Select the conditional attribute", key = f'folder_sec_b_{m}_{n}', label_visibility="collapsed")
                 with grid_sec[2]:
                     idx = 0
